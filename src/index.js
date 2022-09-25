2f26fde7ac88a3df1d69785df109da4f9fb89cfa
David Campos <fernandesc.david@gmail.com>
2022-09-25 12:10:34 -0300

    add routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import Welcome from './pages/Welcome';
+import Dashboard from './pages/Dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Welcome />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Welcome />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
